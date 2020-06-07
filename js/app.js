6773e0e24f10a50ca42660d06833c73131c9d4f2
SSKMBK <60823190+SSKMBK@users.noreply.github.com>
2020-06-07 14:17:09 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -113,182 +113,204 @@ function randomise(){
     weapon_sheet17,weapon_sheet18,weapon_sheet19,weapon_sheet20,weapon_sheet21,weapon_sheet22,
     weapon_sheet23];
 
+    //document.getElementByID("ID").appendChildを使えば書き換えでない追加ができる
     var weaponButton0=document.createTextNode(weapon_sheet0)
-    document.getElementById( "weapon0" ).appendChild( weaponButton0 );
+    document.getElementById( "weapon0" ).innerHTML=weaponButton0.textContent;
     
     var weaponButton1=document.createTextNode(weapon_sheet1)
-    document.getElementById( "weapon1" ).appendChild( weaponButton1 );
+    document.getElementById( "weapon1" ).innerHTML=weaponButton1.textContent;
 
     var weaponButton2=document.createTextNode(weapon_sheet2)
-    document.getElementById( "weapon2" ).appendChild( weaponButton2 );
+    document.getElementById( "weapon2" ).innerHTML=weaponButton2.textContent;
 
     var weaponButton3=document.createTextNode(weapon_sheet3)
-    document.getElementById( "weapon3" ).appendChild( weaponButton3 );
+    document.getElementById( "weapon3" ).innerHTML=weaponButton3.textContent;
 
     var weaponButton4=document.createTextNode(weapon_sheet4)
-    document.getElementById( "weapon4" ).appendChild( weaponButton4 );
+    document.getElementById( "weapon4" ).innerHTML=weaponButton4.textContent;
 
     var weaponButton5=document.createTextNode(weapon_sheet5)
-    document.getElementById( "weapon5" ).appendChild( weaponButton5 );
+    document.getElementById( "weapon5" ).innerHTML=weaponButton5.textContent;
 
     var weaponButton6=document.createTextNode(weapon_sheet6)
-    document.getElementById( "weapon6" ).appendChild( weaponButton6 );
+    document.getElementById( "weapon6" ).innerHTML=weaponButton6.textContent;
 
     var weaponButton7=document.createTextNode(weapon_sheet7)
-    document.getElementById( "weapon7" ).appendChild( weaponButton7 );
+    document.getElementById( "weapon7" ).innerHTML=weaponButton7.textContent;
 
     var weaponButton8=document.createTextNode(weapon_sheet8)
-    document.getElementById( "weapon8" ).appendChild( weaponButton8 );
+    document.getElementById( "weapon8" ).innerHTML=weaponButton8.textContent;
 
     var weaponButton9=document.createTextNode(weapon_sheet9)
-    document.getElementById( "weapon9" ).appendChild( weaponButton9 );
+    document.getElementById( "weapon9" ).innerHTML=weaponButton9.textContent;
 
     var weaponButton10=document.createTextNode(weapon_sheet10)
-    document.getElementById( "weapon10" ).appendChild( weaponButton10 );
+    document.getElementById( "weapon10" ).innerHTML=weaponButton10.textContent;
 
     var weaponButton11=document.createTextNode(weapon_sheet11)
-    document.getElementById( "weapon11" ).appendChild( weaponButton11 );
+    document.getElementById( "weapon11" ).innerHTML=weaponButton11.textContent;
 
     var weaponButton12=document.createTextNode(weapon_sheet12)
-    document.getElementById( "weapon12" ).appendChild( weaponButton12 );
+    document.getElementById( "weapon12" ).innerHTML=weaponButton12.textContent;
 
     var weaponButton13=document.createTextNode(weapon_sheet13)
-    document.getElementById( "weapon13" ).appendChild( weaponButton13 );
+    document.getElementById( "weapon13" ).innerHTML=weaponButton13.textContent;
 
     var weaponButton14=document.createTextNode(weapon_sheet14)
-    document.getElementById( "weapon14" ).appendChild( weaponButton14 );
+    document.getElementById( "weapon14" ).innerHTML=weaponButton14.textContent;
 
     var weaponButton15=document.createTextNode(weapon_sheet15)
-    document.getElementById( "weapon15" ).appendChild( weaponButton15 );
+    document.getElementById( "weapon15" ).innerHTML=weaponButton15.textContent;
 
     var weaponButton16=document.createTextNode(weapon_sheet16)
-    document.getElementById( "weapon16" ).appendChild( weaponButton16 );
+    document.getElementById( "weapon16" ).innerHTML=weaponButton16.textContent;
 
     var weaponButton17=document.createTextNode(weapon_sheet17)
-    document.getElementById( "weapon17" ).appendChild( weaponButton17 );
+    document.getElementById( "weapon17" ).innerHTML=weaponButton17.textContent;
 
     var weaponButton18=document.createTextNode(weapon_sheet18)
-    document.getElementById( "weapon18" ).appendChild( weaponButton18 );
+    document.getElementById( "weapon18" ).innerHTML=weaponButton18.textContent;
 
     var weaponButton19=document.createTextNode(weapon_sheet19)
-    document.getElementById( "weapon19" ).appendChild( weaponButton19 );
+    document.getElementById( "weapon19" ).innerHTML=weaponButton19.textContent;
 
     var weaponButton20=document.createTextNode(weapon_sheet20)
-    document.getElementById( "weapon20" ).appendChild( weaponButton20 );
+    document.getElementById( "weapon20" ).innerHTML=weaponButton20.textContent;
 
     var weaponButton21=document.createTextNode(weapon_sheet21)
-    document.getElementById( "weapon21" ).appendChild( weaponButton21 );
+    document.getElementById( "weapon21" ).innerHTML=weaponButton21.textContent;
 
     var weaponButton22=document.createTextNode(weapon_sheet22)
-    document.getElementById( "weapon22" ).appendChild( weaponButton22 );
+    document.getElementById( "weapon22" ).innerHTML=weaponButton22.textContent;
 
     var weaponButton23=document.createTextNode(weapon_sheet23)
-    document.getElementById( "weapon23" ).appendChild( weaponButton23 );
+    document.getElementById( "weapon23" ).innerHTML=weaponButton23.textContent;
 
 }
 
 function savebutton(){
 
-    if(window.confirm("前回保存したデータは削除されます。")){
+    if(window.confirm("前回保存したデータは上書きされます。")){
 
-        var weapondata=[];
+        var local_list=[];
 
-        weapondata.push(weapon_sheet0,weapon_sheet1,weapon_sheet2,weapon_sheet3,weapon_sheet4,
+        local_list.push(weapon_sheet0,weapon_sheet1,weapon_sheet2,weapon_sheet3,weapon_sheet4,
         weapon_sheet5,weapon_sheet6,weapon_sheet7,weapon_sheet8,weapon_sheet9,weapon_sheet10,
         weapon_sheet11,weapon_sheet12,weapon_sheet13,weapon_sheet14,weapon_sheet15,weapon_sheet16,
         weapon_sheet17,weapon_sheet18,weapon_sheet19,weapon_sheet20,weapon_sheet21,weapon_sheet22,
         weapon_sheet23)
 
-        localStorage.setItem('weapondata_storage', JSON.stringify(weapondata))
-        document.getElementById("save_weapondata").innerHTML=localStorage.getItem('weapondata_storage',);
+        list_message = "[保存済みデータ]\n"+local_list[0]+","+local_list[1]+","+local_list[2]+","+local_list[3]
+        +","+local_list[4]+","+local_list[5]+","+local_list[6]+","+local_list[7]+","+local_list[8]+","
+        +local_list[9]+","+local_list[10]+","+local_list[11]+","+local_list[12]+","+local_list[13]+","
+        +local_list[14]+","+local_list[15]+","+local_list[16]+","+local_list[17]+","+local_list[18]+","
+        +local_list[19]+","+local_list[20]+","+local_list[21]+","+local_list[22]+","+local_list[23];
+
+        localStorage.setItem('salmonrandomweapondata_storage', JSON.stringify(local_list))
+        document.getElementById("save_weapondata").innerHTML=list_message;
 
     }
 
     else{
-        window.alert('キャンセルされました');
     }
 
 
 }
 
 function restorationbutton(){
-
-    var storagedata=JSON.parse(localStorage.getItem('weapondata_storage'))
-
-    var storageweapon0=document.createTextNode(storagedata[0])
-    document.getElementById( "weapon0" ).appendChild( storageweapon0 );
     
-    var storageweapon1=document.createTextNode(storagedata[1])
-    document.getElementById( "weapon1" ).appendChild( storageweapon1 );
+    
+    if(window.confirm("現在の入力内容は上書きされます。")){
 
-    var storageweapon2=document.createTextNode(storagedata[2])
-    document.getElementById( "weapon2" ).appendChild( storageweapon2 );
+        var storagedata=JSON.parse(localStorage.getItem('salmonrandomweapondata_storage'))
 
-    var storageweapon3=document.createTextNode(storagedata[3])
-    document.getElementById( "weapon3" ).appendChild( storageweapon3 );
+        var storageweapon0=document.createTextNode(storagedata[0])
+        document.getElementById( "weapon0" ).innerHTML=storageweapon0.textContent;
 
-    var storageweapon4=document.createTextNode(storagedata[4])
-    document.getElementById( "weapon4" ).appendChild( storageweapon4 );
+        var storageweapon1=document.createTextNode(storagedata[1])
+        document.getElementById( "weapon1" ).innerHTML=storageweapon1.textContent;
 
-    var storageweapon5=document.createTextNode(storagedata[5])
-    document.getElementById( "weapon5" ).appendChild( storageweapon5 );
+        var storageweapon2=document.createTextNode(storagedata[2])
+        document.getElementById( "weapon2" ).innerHTML=storageweapon2.textContent;
 
-    var storageweapon6=document.createTextNode(storagedata[6])
-    document.getElementById( "weapon6" ).appendChild( storageweapon6 );
+        var storageweapon3=document.createTextNode(storagedata[3])
+        document.getElementById( "weapon3" ).innerHTML=storageweapon3.textContent;
 
-    var storageweapon7=document.createTextNode(storagedata[7])
-    document.getElementById( "weapon7" ).appendChild( storageweapon7 );
+        var storageweapon4=document.createTextNode(storagedata[4])
+        document.getElementById( "weapon4" ).innerHTML=storageweapon4.textContent;
 
-    var storageweapon8=document.createTextNode(storagedata[8])
-    document.getElementById( "weapon8" ).appendChild( storageweapon8 );
+        var storageweapon5=document.createTextNode(storagedata[5])
+        document.getElementById( "weapon5" ).innerHTML=storageweapon5.textContent;
 
-    var storageweapon9=document.createTextNode(storagedata[9])
-    document.getElementById( "weapon9" ).appendChild( storageweapon9 );
+        var storageweapon6=document.createTextNode(storagedata[6])
+        document.getElementById( "weapon6" ).innerHTML=storageweapon6.textContent;
 
-    var storageweapon10=document.createTextNode(storagedata[10])
-    document.getElementById( "weapon10" ).appendChild( storageweapon10 );
+        var storageweapon7=document.createTextNode(storagedata[7])
+        document.getElementById( "weapon7" ).innerHTML=storageweapon7.textContent;
 
-    var storageweapon11=document.createTextNode(storagedata[11])
-    document.getElementById( "weapon11" ).appendChild( storageweapon11 );
+        var storageweapon8=document.createTextNode(storagedata[8])
+        document.getElementById( "weapon8" ).innerHTML=storageweapon8.textContent;
 
-    var storageweapon12=document.createTextNode(storagedata[12])
-    document.getElementById( "weapon12" ).appendChild( storageweapon12 );
+        var storageweapon9=document.createTextNode(storagedata[9])
+        document.getElementById( "weapon9" ).innerHTML=storageweapon9.textContent;
 
-    var storageweapon13=document.createTextNode(storagedata[13])
-    document.getElementById( "weapon13" ).appendChild( storageweapon13 );
+        var storageweapon10=document.createTextNode(storagedata[10])
+        document.getElementById( "weapon10" ).innerHTML=storageweapon10.textContent;
 
-    var storageweapon14=document.createTextNode(storagedata[14])
-    document.getElementById( "weapon14" ).appendChild( storageweapon14 );
+        var storageweapon11=document.createTextNode(storagedata[11])
+        document.getElementById( "weapon11" ).innerHTML=storageweapon11.textContent;
 
-    var storageweapon15=document.createTextNode(storagedata[15])
-    document.getElementById( "weapon15" ).appendChild( storageweapon15 );
+        var storageweapon12=document.createTextNode(storagedata[12])
+        document.getElementById( "weapon12" ).innerHTML=storageweapon12.textContent;
 
-    var storageweapon16=document.createTextNode(storagedata[16])
-    document.getElementById( "weapon16" ).appendChild( storageweapon16 );
+        var storageweapon13=document.createTextNode(storagedata[13])
+        document.getElementById( "weapon13" ).innerHTML=storageweapon13.textContent;
 
-    var storageweapon17=document.createTextNode(storagedata[17])
-    document.getElementById( "weapon17" ).appendChild( storageweapon17 );
+        var storageweapon14=document.createTextNode(storagedata[14])
+        document.getElementById( "weapon14" ).innerHTML=storageweapon14.textContent;
 
-    var storageweapon18=document.createTextNode(storagedata[18])
-    document.getElementById( "weapon18" ).appendChild( storageweapon18 );
+        var storageweapon15=document.createTextNode(storagedata[15])
+        document.getElementById( "weapon15" ).innerHTML=storageweapon15.textContent;
 
-    var storageweapon19=document.createTextNode(storagedata[19])
-    document.getElementById( "weapon19" ).appendChild( storageweapon19 );
+        var storageweapon16=document.createTextNode(storagedata[16])
+        document.getElementById( "weapon16" ).innerHTML=storageweapon16.textContent;
 
-    var storageweapon20=document.createTextNode(storagedata[20])
-    document.getElementById( "weapon20" ).appendChild( storageweapon20 )
+        var storageweapon17=document.createTextNode(storagedata[17])
+        document.getElementById( "weapon17" ).innerHTML=storageweapon17.textContent;
 
-    var storageweapon21=document.createTextNode(storagedata[21])
-    document.getElementById( "weapon21" ).appendChild( storageweapon21 );
+        var storageweapon18=document.createTextNode(storagedata[18])
+        document.getElementById( "weapon18" ).innerHTML=storageweapon18.textContent;
 
-    var storageweapon22=document.createTextNode(storagedata[22])
-    document.getElementById( "weapon22" ).appendChild( storageweapon22 );
+        var storageweapon19=document.createTextNode(storagedata[19])
+        document.getElementById( "weapon19" ).innerHTML=storageweapon19.textContent;
 
-    var storageweapon23=document.createTextNode(storagedata[23])
-    document.getElementById( "weapon23" ).appendChild( storageweapon23 );
+        var storageweapon20=document.createTextNode(storagedata[20])
+        document.getElementById( "weapon20" ).innerHTML=storageweapon20.textContent;
 
+        var storageweapon21=document.createTextNode(storagedata[21])
+        document.getElementById( "weapon21" ).innerHTML=storageweapon21.textContent;
 
+        var storageweapon22=document.createTextNode(storagedata[22])
+        document.getElementById( "weapon22" ).innerHTML=storageweapon22.textContent;
 
+        var storageweapon23=document.createTextNode(storagedata[23])
+        document.getElementById( "weapon23" ).innerHTML=storageweapon23.textContent;
+    
+    }
 
+    else{
+    }
 
+}
+
+function deletebutton(){
+    if(window.confirm("本当に保存データを削除しますか？")){
+
+        localStorage.removeItem("salmonrandomweapondata_storage")
+        document.getElementById("save_weapondata").innerHTML="削除しました";
+
+    }
+
+    else{
+    }
 }
